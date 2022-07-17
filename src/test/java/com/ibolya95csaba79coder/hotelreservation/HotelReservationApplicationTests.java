5b5adb492936885d3fda348726edc4ced6b721aa
Csaba79-coder <csabavadasz79@gmail.com>
2022-07-17 14:17:57 +0200

    make CI setup - runs properly with @SpringBootTest annotation! gradle builds successfully!

--- src/test/java/com/ibolya95csaba79coder/hotelreservation/HotelReservationApplicationTests.java
+++ src/test/java/com/ibolya95csaba79coder/hotelreservation/HotelReservationApplicationTests.java
@@ -1,12 +1,7 @@
 package com.ibolya95csaba79coder.hotelreservation;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
 class HotelReservationApplicationTests {
 
     @Test
