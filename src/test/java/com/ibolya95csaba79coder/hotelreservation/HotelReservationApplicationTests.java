176440a2914286ae0aa65632dabab974a349f686
Csaba79-coder <csabavadasz79@gmail.com>
2022-07-17 12:57:31 +0200

    make CI setup - add SpringBootTest annotation to test class

--- src/test/java/com/ibolya95csaba79coder/hotelreservation/HotelReservationApplicationTests.java
+++ src/test/java/com/ibolya95csaba79coder/hotelreservation/HotelReservationApplicationTests.java
@@ -1,7 +1,9 @@
 package com.ibolya95csaba79coder.hotelreservation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class HotelReservationApplicationTests {
 
     @Test
