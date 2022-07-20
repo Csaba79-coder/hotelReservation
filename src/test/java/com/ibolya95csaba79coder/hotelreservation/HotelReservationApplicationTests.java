69d8d805a571d0a47cd4ed411b2275aa2528d6cb
Csaba79-coder <csabavadasz79@gmail.com>
2022-07-20 16:09:50 +0200

    ci config in test env.

--- src/test/java/com/ibolya95csaba79coder/hotelreservation/HotelReservationApplicationTests.java
+++ src/test/java/com/ibolya95csaba79coder/hotelreservation/HotelReservationApplicationTests.java
@@ -1,10 +1,17 @@
 package com.ibolya95csaba79coder.hotelreservation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class HotelReservationApplicationTests {
 
     @Test
     void contextLoads() {
     }
 }
+// https://stackoverflow.com/questions/44764151/springboot-tests-using-application-properties-with-environment-variables
+
+// https://stackoverflow.com/questions/11306951/how-to-set-environment-variable-or-system-property-in-spring-tests
+
+// https://www.baeldung.com/spring-tests-override-properties
